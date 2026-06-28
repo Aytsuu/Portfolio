b86a343ff228e0048fb380bc6b6d5c497369ce82
Aytsuu <paoloaraneta008@gmail.com>
2026-06-28 21:34:47 +0800

    Update

--- v1/src/dto/experiences.dto.ts
+++ v1/src/dto/experiences.dto.ts
@@ -13,11 +13,6 @@ export const experiences: props[] = [
     experiencedAt: 'Dinnox IT Solutions',
     year: 'Feb 2026 - June 2026',
     location: 'Cebu City, Philippines',
-    highlights: [
-      'Led the development of a client-based website, implemented a structured monolithic architecture, and designed user workflows.',
-      'Fixed a backend bug that heavily affected the development workflow of interns and senior developers.',
-      'Increased team productivity by introducing automation tools and agentic engineering workflows.'
-    ],
     skills: []
   },
   {
@@ -25,11 +20,6 @@ export const experiences: props[] = [
     experiencedAt: 'Barangay San Roque (Ciudad)',
     year: 'Aug 2025 - Oct 2025',
     location: 'Cebu City, Philippines',
-    highlights: [
-      'Served as a student trainer for the BarangayConnect Training Program, supporting 50+ local government workers through hands-on digital literacy training.',
-      'Introduced and demonstrated key features of the barangay system to non-technical residents and staff.',
-      'Conducted practical training sessions to assess and improve computer literacy and system usage skills.'
-    ],
     skills: []
   },
   {
